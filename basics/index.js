6e5ec3ffb59135a17929eea80f8e5a925426562f
Yahya Hussein <yahyahussein91@gmail.com>
2018-12-09 16:38:13 -0800

    added notes for diff datatypes

--- basics/index.js
+++ basics/index.js
@@ -8,7 +8,19 @@
 	// Has a bunch of frameworks and libraries
 	// Different versions such as es5/6/2015/2017 etc
 
-	
+// A variable is a container that has a value inside which can be used over and over again
+// Declare a variable
+	// 'var' keyword
+	// name of variable
+	// '=' what you want the value to be
+
+// There are 5 different datatypes
+	// Number - Floating point numbers, for decimals and integers
+	// String - Sequence of characters, used for text
+	// Boolean - Logical data type that can only be true or false
+	// Undefined - Data type of a variable that does not have a value yet
+	// Null - Also means 'non existent'
+var firstName = 'John';	
 
 // Write to the console
-console.log("Hello World");
+console.log(firstName);
