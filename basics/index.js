76784f931c94fc0dbd03ffe0a3d132d0f1ae7402
Yahya Hussein <yahyahussein91@gmail.com>
2018-12-09 16:29:40 -0800

    adding comments about js

---
+++ basics/index.js
@@ -0,0 +1,9 @@
+// Basics of JavaScript
+// What is javascript?
+	// Lightweight meaning it doesn't take up much memory/simple syntax and features
+	// Cross-platform meaning it can be used on multiple systems
+	// Object-oriented
+	// Used to only be a client side language
+
+//Write to the console
+console.log("Hello World");
