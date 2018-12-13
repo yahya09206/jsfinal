f49703e0072fe5ce96382f5dbbe934bbaf5fdde8
Yahya Hussein <yahyahussein91@gmail.com>
2018-12-13 10:41:25 -0800

    finished with challenge

--- basics/challenges.js
+++ basics/challenges.js
@@ -1,5 +1,8 @@
-var markHeight = 67;
-var johnHeight = 54;
+// Challenge 1
+var markHeight = 6.7;
+var johnHeight = 5.4;
 var johnBMI = 125 / (johnHeight * johnHeight);
 var markBMI = 135 / (markHeight * markHeight);
 
+var result = johnBMI < markBMI;
+console.log(result);
