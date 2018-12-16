240824db22a62ba678ab73075a7a10c235015076
Yahya Hussein <yahyahussein91@gmail.com>
2018-12-15 17:20:56 -0800

    ternary and switch staements

--- basics/index.js
+++ basics/index.js
@@ -117,4 +117,36 @@ Control Structures
 var firstName = 'John';
 var civilStatus = 'single';
 
+if (civilStatus === 'married') {
+	console.log(firstName + ' is married');
+} else {
+	console.log(firstName + ' will hopefully marry soon :)');
+}
+
+var isMarried = true;
+if (isMarried === true) {
+	console.log(firstName + ' is married');
+} else {
+	console.log(firstName + ' will hopefully marry soon :)');
+}
+
+/******************
+Boolean Logic
+******************/
+
+var firstName = 'John';
+var age = 16;
+if(age < 13){
+	console.log(firstName + ' is a boy.');
+}else if (age >= 13 && age < 20 ) {
+	console.log(firstName + ' is a teenager.');
+}else{
+	console.log(firstName + ' is a man.');
+}
+
+/******************
+Ternary Operator & switch statements
+******************/
+
+
 
