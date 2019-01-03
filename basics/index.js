7f457eeb7ef8f686fb7ff60f04e70124194eecc8
Yahya Hussein <yahyahussein91@gmail.com>
2019-01-03 00:05:53 -0800

    ternary op

--- basics/index.js
+++ basics/index.js
@@ -147,6 +147,13 @@ if(age < 13){
 /******************
 Ternary Operator & switch statements
 ******************/
+var firstName = 'John';
+var age = 16;
+
+age >= 18 ? console.log(firstName + ' drinks beer.') : console.log(firstName + ' drinks juice.');
+
+var drink = age >= 18 ? 'beer' : 'juice';
+console.log(drink);
 
 
 
