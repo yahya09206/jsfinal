46c63769c28d5992dc1af5b3663b7b7d8f5d814c
Yahya Hussein <yahyahussein91@gmail.com>
2019-01-03 00:45:52 -0800

    finished with challenge 2

--- basics/challenges.js
+++ basics/challenges.js
@@ -8,4 +8,22 @@ var result = johnBMI < markBMI;
 console.log(result);
 
 // Challenge 2
+var teamMike = (89+120+103)/3;
+var teamJohn = (116+94+123)/3;
+var teamMary = (97+134+105)/3;
+
+switch(true){
+	case teamMike > teamJohn && teamMike > teamMary :
+		console.log('Mike is the winner with ' + teamMike + ' average points');
+		break;
+	case teamJohn > teamMike && teamJohn > teamMary :
+		console.log('John is the winner with ' + teamJohn + ' average points');
+		break;
+	case teamMary > teamJohn && teamMary > teamMike :
+		console.log('Mary is the winner with ' + teamMary + ' average points');
+		break;
+	default:
+		console.log('TIE!!!!');
+
+}
 
