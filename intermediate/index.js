e3a331e0faed9cc4e75e7f4ba7335f54d1fedf12
Yahya Hussein <yahyahussein91@gmail.com>
2019-01-05 13:40:34 -0800

    done with for loop

--- intermediate/index.js
+++ intermediate/index.js
@@ -111,7 +111,13 @@ john.calcAge();
 console.log(john);
 
 //Loops and iterations
+for(var i = 0; i < 10; i++){
+	console.log(i);
+}
 
-
+var john = ['John', 'Smith', 1990, 'teacher', false];
+for (var i = 0; i < john.length; i++) {
+	console.log(john[i]);
+}
 
 
