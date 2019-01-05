31a10fa74f4d1cb1f2bc9ff273346dd4b6bd7043
Yahya Hussein <yahyahussein91@gmail.com>
2019-01-04 20:50:00 -0800

    finished with challenge

--- intermediate/challenges.js
+++ intermediate/challenges.js
@@ -15,6 +15,8 @@ console.log(tipCalculator(10));
 var bills = [124, 48, 268];
 var tips = [tipCalculator(bills[0]),
 			tipCalculator(bills[1]),
-			tipCalculator(bills[2]);
-
-console.log(tips);
+			tipCalculator(bills[2])];
+var finalValue = [bills[0] + tips[0],
+				  bills[1] + tips[1],
+				  bills[2] + tips[2]];
+console.log(tips, finalValue);
