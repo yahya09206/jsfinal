1d5297f03c4d88875ee460b222761d1bbeffb6b7
Yahya Hussein <yahyahussein91@gmail.com>
2019-01-06 20:15:56 -0800

    more hoisting

--- advanced/index.js
+++ advanced/index.js
@@ -1,5 +1,15 @@
 //Hoisting
+	//only works with function declarations
+calculateAge(1965);
 function calculateAge(year){
-	console.log(2016-year);
+	console.log(2018 - year);
 }
-calculateAge(1991);
+
+//retirement(1965);
+var retirement = function(year){
+	console.log(65 - (2018 - year));
+}
+
+// variables
+var age = 23;
+console.log(age);
