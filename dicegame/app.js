ca949cdf4fb1addeabee7a4d5c48cd232809a60b
Yahya Hussein <yahyahussein91@gmail.com>
2019-01-11 00:53:47 -0800

    done with main function of game

--- dicegame/app.js
+++ dicegame/app.js
@@ -76,22 +76,29 @@ function nextPlayer(){
 }
 
 //New game initialization
-document.querySelector('.btn-new').addEventListener('click', init());
+document.querySelector('.btn-new').addEventListener('click', init);
 
 //Init function
 function init(){
 	scores = [0,0];
 	roundScore = 0;
 	activePlayer = 0;
-
 	//Use query selector to change element of css
 	document.querySelector('.dice').style.display = 'none';
-
 	//User getelementbyid
 	document.getElementById('score-0').textContent = '0';
 	document.getElementById('score-1').textContent = '0';
 	document.getElementById('current-0').textContent = '0';
 	document.getElementById('current-1').textContent = '0';
+	document.getElementById('name-0').textContent = 'Player 1';
+	document.getElementById('name-1').textContent = 'Player 2';
+	document.querySelector('.player-0-panel').classList.remove('winner');
+	document.querySelector('.player-1-panel').classList.remove('winner');
+	document.querySelector('.player-0-panel').classList.remove('active');
+	document.querySelector('.player-1-panel').classList.remove('active');
+	document.querySelector('.player-0-panel').classList.add('active');
+
+
 }
 
 
