267ef3346d6d7239f964ad4ccf4543bbba238b9c
Yahya Hussein <yahyahussein91@gmail.com>
2019-01-13 22:47:28 -0800

    removed comments

--- dicegame/app.js
+++ dicegame/app.js
@@ -10,48 +10,50 @@ GAME RULES:
 */
 
 // Variables for game
-var scores, roundScore, activePlayer;
+var scores, roundScore, activePlayer, gamePlaying;
 init();
 
 // Hide dice on page load
 document.querySelector('.btn-roll').addEventListener('click', function(){
-	//1. random number
-		// Generate random number for dice
-		var dice = Math.floor(Math.random() * 6) + 1;
-
-	//2. display result
-		var diceDOM = document.querySelector('.dice');
-		// match dice result with match picture
-		diceDOM.style.display = 'block';
-		diceDOM.src = 'dice-' + dice + '.png';
-
-	//3. update round score only if rolled number was not a 1
-		if (dice !== 1) {
-			//Add score
-			roundScore += dice;
-			//Display roundScore
-			document.querySelector('#current-' + activePlayer).textContent = roundScore;
-		}else{
-			nextPlayer();
-
-		}
+	if (gamePlaying) {
+		//1. random number
+			// Generate random number for dice
+			var dice = Math.floor(Math.random() * 6) + 1;
+		//2. display result
+			var diceDOM = document.querySelector('.dice');
+			// match dice result with match picture
+			diceDOM.style.display = 'block';
+			diceDOM.src = 'dice-' + dice + '.png';
+		//3. update round score only if rolled number was not a 1
+			if (dice !== 1) {
+				//Add score
+				roundScore += dice;
+				//Display roundScore
+				document.querySelector('#current-' + activePlayer).textContent = roundScore;
+			}else{
+				nextPlayer();
+			}
+	}
 });
 
 //Button hold function
 document.querySelector('.btn-hold').addEventListener('click', function(){
-	//Add current score to players global score
-	scores[activePlayer] += roundScore;
-	//Update UI
-	document.querySelector('#score-' + activePlayer).textContent = scores[activePlayer];
-	//Check if player won the game
-	if (scores[activePlayer] >= 20) {
-		document.querySelector('#name-' + activePlayer).textContent = 'Winner!';
-		document.querySelector('.dice').style.display = 'none';
-		document.querySelector('.player-' + activePlayer + '-panel').classList.add('winner');
-		document.querySelector('.player-' + activePlayer + '-panel').classList.remove('active');
-	}else{
-		//Next player
-		nextPlayer();
+	if (gamePlaying) {
+		//Add current score to players global score
+		scores[activePlayer] += roundScore;
+		//Update UI
+		document.querySelector('#score-' + activePlayer).textContent = scores[activePlayer];
+		//Check if player won the game
+		if (scores[activePlayer] >= 20) {
+			document.querySelector('#name-' + activePlayer).textContent = 'Winner!';
+			document.querySelector('.dice').style.display = 'none';
+			document.querySelector('.player-' + activePlayer + '-panel').classList.add('winner');
+			document.querySelector('.player-' + activePlayer + '-panel').classList.remove('active');
+			gamePlaying = false;
+		}else{
+			//Next player
+			nextPlayer();
+		}
 	}
 });
 
@@ -68,9 +70,6 @@ function nextPlayer(){
 	document.querySelector('.player-1-panel').classList.toggle('active');
 
 	//Remove active class
-	// document.querySelector('.player-0-panel').classList.remove('active');
-	//Add active class to other panel
-	// document.querySelector('.player-1-panel').classList.add('active');
 	//Hide dice when player rolls a 1
 	document.querySelector('.dice').style.display = 'none';
 }
@@ -83,6 +82,7 @@ function init(){
 	scores = [0,0];
 	roundScore = 0;
 	activePlayer = 0;
+	gamePlaying = true;
 	//Use query selector to change element of css
 	document.querySelector('.dice').style.display = 'none';
 	//User getelementbyid
@@ -97,8 +97,6 @@ function init(){
 	document.querySelector('.player-0-panel').classList.remove('active');
 	document.querySelector('.player-1-panel').classList.remove('active');
 	document.querySelector('.player-0-panel').classList.add('active');
-
-
 }
 
 
