a35e9547d787f834ba0044ea99da1ac7b164bd15
Yahya Hussein <yahyahussein91@gmail.com>
2019-07-27 11:39:21 -0700

    added inheritance

--- advanObjFunc/index.js
+++ advanObjFunc/index.js
@@ -11,4 +11,12 @@ var Person = function(name, yearOfBirth, job) {
 	this.name = name;
 	this.yearOfBirth = yearOfBirth;
 	this.job = job;
+	this.calculateAge = function() {
+		console.log(2019 - this.yearOfBirth);
+	}
 }
+
+// Create new john object using constructor 
+var john = new Person('John', 1990, 'Teacher');
+john.calculateAge();
+
