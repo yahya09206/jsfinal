a3a0ab01de5ea7ecece254962eb5ad2ad84fea62
Yahya Hussein <yahyahussein91@gmail.com>
2019-07-27 11:42:40 -0700

    prototype chainig

--- advanObjFunc/index.js
+++ advanObjFunc/index.js
@@ -11,12 +11,21 @@ var Person = function(name, yearOfBirth, job) {
 	this.name = name;
 	this.yearOfBirth = yearOfBirth;
 	this.job = job;
-	this.calculateAge = function() {
-		console.log(2019 - this.yearOfBirth);
-	}
+	// this.calculateAge = function() {
+	// 	console.log(2019 - this.yearOfBirth);
+	// }
+}
+
+Person.prototype.calculateAge  = function() {
+	console.log(2019 - this.yearOfBirth);
 }
 
 // Create new john object using constructor 
 var john = new Person('John', 1990, 'Teacher');
 john.calculateAge();
+
+var jane = new Person('Jane', 1969, 'Designer');
+var mark = new Person('Mark', 1948, 'Retired');
+jane.calculateAge();
+mark.calculateAge();
 
