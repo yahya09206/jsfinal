4f7d1784b2289aaac29f862521ebb0bf9a7ea741
Yahya Hussein <yahyahussein91@gmail.com>
2019-07-27 11:44:48 -0700

    prototyping

--- advanObjFunc/index.js
+++ advanObjFunc/index.js
@@ -20,6 +20,8 @@ Person.prototype.calculateAge  = function() {
 	console.log(2019 - this.yearOfBirth);
 }
 
+Person.prototype.lastName = 'Smith';
+
 // Create new john object using constructor 
 var john = new Person('John', 1990, 'Teacher');
 john.calculateAge();
@@ -28,4 +30,8 @@ var jane = new Person('Jane', 1969, 'Designer');
 var mark = new Person('Mark', 1948, 'Retired');
 jane.calculateAge();
 mark.calculateAge();
+
+console.log(john.lastName);
+console.log(jane.lastName);
+console.log(mark.lastName);
 
