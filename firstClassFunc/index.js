65214fac9652c71ef891c45b6323f1061a7e0cbb
Yahya Hussein <yahyahussein91@gmail.com>
2019-07-27 12:27:39 -0700

    fixed array error

---
+++ firstClassFunc/index.js
@@ -0,0 +1,20 @@
+// First class functions
+var years  = [1990, 1965, 1937, 1991, 2006];
+
+function arrayCalc(arr, fn){
+	//loop over array
+	//Create empty array
+	var arrRes = [];
+	for (var i = 0; i < arr.length; i++) {
+		// Push result of fn and pass current input array
+		arrRes.push(fn(arr[i]));
+	}
+	return arrRes;
+}
+
+function calculateAge(el) {
+	return 2019 - el;
+}
+
+var ages = arrayCalc(years, calculateAge);
+console.log(ages);
