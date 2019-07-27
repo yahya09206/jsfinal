c24d05f01d139010f9c04f22c444d9981be5a67e
Yahya Hussein <yahyahussein91@gmail.com>
2019-07-27 12:38:13 -0700

    functions that return functions

--- firstClassFunc/index.js
+++ firstClassFunc/index.js
@@ -1,37 +1,52 @@
-// First class functions
-var years  = [1990, 1965, 1937, 1991, 2006];
+// // First class functions
+// var years  = [1990, 1965, 1937, 1991, 2006];
 
-function arrayCalc(arr, fn){
-	//loop over array
-	//Create empty array
-	var arrRes = [];
-	for (var i = 0; i < arr.length; i++) {
-		// Push result of fn and pass current input array
-		arrRes.push(fn(arr[i]));
+// function arrayCalc(arr, fn){
+// 	//loop over array
+// 	//Create empty array
+// 	var arrRes = [];
+// 	for (var i = 0; i < arr.length; i++) {
+// 		// Push result of fn and pass current input array
+// 		arrRes.push(fn(arr[i]));
+// 	}
+// 	return arrRes;
+// }
+
+// function calculateAge(el) {
+// 	return 2019 - el;
+// }
+
+// function isFullAge(el) {
+// 	return el >= 18;
+// }
+
+// function maxHeartRate(el) {
+// 	if (el >= 18 && el <= 81) {
+// 		return Math.round(206.9 - (0.67 * el));
+// 	} else {
+// 		return -1;
+// 	}
+// }
+
+// var ages = arrayCalc(years, calculateAge);
+// var fullAges = arrayCalc(ages, isFullAge);
+// var rates = arrayCalc(ages, maxHeartRate);
+
+// console.log(ages);
+// console.log(rates);
+
+// Functions that return functions
+function interviewQuestion(job) {
+	if (job === 'designer') {
+		return function(name) {
+			console.log(name + ', can you please explain what UX design is?');
+		} else if (job === 'teacher'){
+			console.log(name + ', what subject do you teach');
+		}
+	}else {
+		return function(name){
+			console.log('Hello,' + name + '. What do you do?');
+		}
 	}
-	return arrRes;
 }
-
-function calculateAge(el) {
-	return 2019 - el;
-}
-
-function isFullAge(el) {
-	return el >= 18;
-}
-
-function maxHeartRate(el) {
-	if (el >= 18 && el <= 81) {
-		return Math.round(206.9 - (0.67 * el));
-	} else {
-		return -1;
-	}
-}
-
-var ages = arrayCalc(years, calculateAge);
-var fullAges = arrayCalc(ages, isFullAge);
-var rates = arrayCalc(ages, maxHeartRate);
-
-console.log(ages);
-console.log(rates);
 
