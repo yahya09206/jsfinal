71b70655e4e27691de7935e005860d7cb72e24cc
Yahya Hussein <yahyahussein91@gmail.com>
2019-07-27 11:34:12 -0700

    function constructor for person object

--- advanObjFunc/index.js
+++ advanObjFunc/index.js
@@ -1,1 +1,14 @@
 // Advance Objects and Functions
+
+// Function Constructor
+var john = {
+	name: 'John',
+	yearOfBirth: 1991,
+	job: 'teacher'
+};
+
+var Person = function(name, yearOfBirth, job) {
+	this.name = name;
+	this.yearOfBirth = yearOfBirth;
+	this.job = job;
+}
