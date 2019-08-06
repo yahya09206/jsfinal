0ceaaa9e5564f9ba59eab703324a2dc5fbb637f9
Yahya Hussein <yahyahussein91@gmail.com>
2019-08-05 19:55:38 -0700

    used bind method to attach this keyword to function

--- es6/script.js
+++ es6/script.js
@@ -122,3 +122,17 @@ const box6 = {
 }
 
 box6.clickMe();
+
+function Person(name){
+	this.name = name;
+}
+Person.prototype.myFriends5 = function(friends){
+	var arr = friends.map(function(el) 
+	{
+		return this.name + ' is friends with ' + el;
+	}.bind(this));
+	
+	console.log(arr);
+}
+var friends = ['Bob', 'Jane', 'Mark'];
+new Person('John').myFriends5(friends);
