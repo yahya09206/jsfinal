0405e3798ee568552d62538fb0af7a83be0b7aa0
Yahya Hussein <yahyahussein91@gmail.com>
2019-08-07 19:05:02 -0700

    done with spread

--- es6/script.js
+++ es6/script.js
@@ -216,6 +216,38 @@ console.log(ages.findIndex(cur => cur >= 18));
 console.log(ages.find(cur => cur >= 18));
 
 // Spread Operator
+function addFourAges (a, b, c, d) {
+	return a + b + c + d;
+}
+var sum1 = addFourAges(18, 30, 12, 21);
+console.log(sum1);
+
+// Pass entire array into function
+var ages = [18, 30, 12, 21];
+// Use apply method and then call function using params
+var sum2 = addFourAges.apply(null, ages);
+console.log(sum2);
+
+// ES6 using spread operator
+const sum3 = addFourAges(...ages);
+console.log(sum3);
+
+const familySmith = ['John', 'Jane', 'Mark'];
+const familyMiller = ['Mary', 'Bob', 'Ann'];
+const bigFamily = [...familySmith, ...familyMiller];
+console.log(bigFamily);
+
+// Use spread on nodelist
+const h = document.querySelector('h1');
+const boxes = document.querySelectorAll('.box');
+const all = [h, ...boxes];
+
+Array.from(all).forEach(cur => cur.style.color = 'purple');
+
+
+
+
+
 
 
 
