610ba6cbc1caf5d4394b6a614b2510efc79baa63
Yahya Hussein <yahyahussein91@gmail.com>
2019-12-17 15:29:26 -0800

    starting on challenge

---
+++ varsAndFlowControl/script.js
@@ -0,0 +1,12 @@
+// 'let' => new variable name declaration, reserved keyword
+// String starts with a single qoute and ends with a single quote
+let name = 'Yahya Hussein';
+let firstName = 'Yahya';
+let lastName = 'Hussein';
+let fullName = firstName + ' ' + lastName;
+
+// Print out of the contents of the variable 'name'
+console.log(name);
+console.log(fullName);
+
+//Challenge 1
