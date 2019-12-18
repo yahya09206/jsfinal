74e56965b1bb025022f57080c99a87895c35b3ad
Yahya Hussein <yahyahussein91@gmail.com>
2019-12-18 15:43:25 -0800

    done with challenge 2

--- functions/params.js
+++ functions/params.js
@@ -1,6 +1,22 @@
-let add = function (a, b) {
-	return a + b;
+// Multiple Arguments
+let add = function (a, b, c) {
+	return a + b + c;
 }
 
-let result = add(10,1);
+let result = add(10,1, 5);
 console.log(result);
+
+// Default Arguments
+let getScoreText = function (name = 'Anonymous', score = 0) {
+	return 'Name: ' + name + ' Score: ' + score;
+}
+
+let scoreText = getScoreText(undefined, 4);
+console.log(scoreText);
+
+// Mulltiple params challenge 2
+let getTip = function(total, percentage = .3){
+	let tip = total * percentage;
+	return tip;
+}
+console.log(getTip(40));
