08963c866517994101b48bd6ccdec423931ee2a1
Yahya Hussein <yahyahussein91@gmail.com>
2019-12-17 16:28:02 -0800

    challenge complete

--- varsAndFlowControl/booleans.js
+++ varsAndFlowControl/booleans.js
@@ -1,17 +1,17 @@
-let temp = 31;
-let isFreezing = temp < 32;
+// let temp = 31;
+// let isFreezing = temp < 32;
 
-// Condition using if statements
-if (isFreezing) {
-	console.log('It is freezing outside');
-}else {
-	console.log("It is friggin hot");
-}
+// // Condition using if statements
+// if (isFreezing) {
+// 	console.log('It is freezing outside');
+// }else {
+// 	console.log("It is friggin hot");
+// }
 
-if (temp >= 110) {
-	console.log('It is way to hot outisde');
-	console.log('Testing123');
-}
+// if (temp >= 110) {
+// 	console.log('It is way to hot outisde');
+// 	console.log('Testing123');
+// }
 
 //If statement challenge
 let age = 65;
@@ -20,4 +20,23 @@ if (age <= 7) {
 	console.log('This is for child pricing of 20% off');
 }if (age >= 65) {
 	console.log('This is for older people 25% off');
+}
+
+// Using else
+let isAccountLocked = true;
+let userRole = 'admin';
+
+if (isAccountLocked) {
+	console.log('Account is locked');
+}else {
+	console.log('welcome');
+}
+
+let temp = 45;
+if (temp <= 45) {
+	console.log('It is freezing outside');
+} else if (temp > 45) {
+	console.log('It is hot outside');
+} else {
+	console.log('Go for it. It is pretty nice');
 }
