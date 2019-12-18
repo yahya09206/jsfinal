364ba6472e40f78fd93e58ea9d679dc401d26601
Yahya Hussein <yahyahussein91@gmail.com>
2019-12-17 16:28:45 -0800

    fixed challenge

--- varsAndFlowControl/booleans.js
+++ varsAndFlowControl/booleans.js
@@ -33,9 +33,9 @@ if (isAccountLocked) {
 }
 
 let temp = 45;
-if (temp <= 45) {
+if (temp <= 32) {
 	console.log('It is freezing outside');
-} else if (temp > 45) {
+} else if (temp >= 110) {
 	console.log('It is hot outside');
 } else {
 	console.log('Go for it. It is pretty nice');
