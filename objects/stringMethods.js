9242db5469c4d521c66e395ca8697dcfca55c6b5
Yahya Hussein <yahyahussein91@gmail.com>
2019-12-19 18:07:05 -0800

    done with string method challenge

--- objects/stringMethods.js
+++ objects/stringMethods.js
@@ -12,3 +12,15 @@ console.log(password.includes('password'));
 
 // Trim method
 console.log(name.trim());
+
+let passwordCheck = function(password){
+	if (password.length > 8 && !password.includes('password')) {
+		return true;
+	}else {
+		return false;
+	}
+}
+
+console.log(passwordCheck('asdfp'));
+console.log(passwordCheck('abc123!@#$%^&'));
+console.log(passwordCheck('password'));
