ba342b03487b780839ed8fd08f83030c541fb96e
Yahya Hussein <yahyahussein91@gmail.com>
2019-12-19 17:46:39 -0800

    finished with object method challenge

---
+++ objects/objectMethods.js
@@ -0,0 +1,21 @@
+let restaurant = {
+	name: 'Moctezumas',
+	guestCapacity: 75,
+	guestCount: 0,
+	checkAvailability: function(partySize){
+		let seatsLeft = this.guestCapacity - this.guestCount;
+		return partySize <= seatsLeft;
+	},
+	seatParty: function(partySize){
+		this.guestCount += partySize;
+	},
+	removeParty: function(partySize){
+		this.guestCount -= partySize;
+	}
+};
+
+restaurant.seatParty(72);
+console.log(restaurant.checkAvailability(4));
+restaurant.removeParty(5);
+console.log(restaurant.checkAvailability(4));
+
