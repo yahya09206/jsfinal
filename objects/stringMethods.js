e61ce6d70a5994bdfe767793a9e7ecf134698617
Yahya Hussein <yahyahussein91@gmail.com>
2019-12-19 17:59:36 -0800

    playing around with diff javascript string methods

---
+++ objects/stringMethods.js
@@ -0,0 +1,14 @@
+let name = '  Andrew Mead'
+
+// length property
+console.log(name.length);
+
+// Convert to uppercase
+console.log(name.toUpperCase());
+
+// Includes method
+let password = 'ab123afasd098';
+console.log(password.includes('password'));
+
+// Trim method
+console.log(name.trim());
