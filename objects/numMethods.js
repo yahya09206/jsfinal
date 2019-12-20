181a5589ec0eb7cb784483db0ebf7e9e650377e9
Yahya Hussein <yahyahussein91@gmail.com>
2019-12-19 18:26:24 -0800

    fixed extra parens

--- objects/numMethods.js
+++ objects/numMethods.js
@@ -1,8 +1,8 @@
 // challenge
 let makeGuess = function(num){
-	let min = 0;
+	let min = 1;
 	let max = 5;
-	let guess = Math.floor(Math.random() * (max - min + 1) + min);
+	let guess = Math.floor(Math.random() * (max - min + 1)) + min;
 	if (num === guess) {
 		return true;
 	} else {
@@ -11,7 +11,3 @@ let makeGuess = function(num){
 }
 
 console.log(makeGuess(2));
-console.log(makeGuess(3));
-console.log(makeGuess(4));
-console.log(makeGuess(5));
-console.log(makeGuess(1));
