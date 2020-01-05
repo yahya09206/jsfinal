7cbc0bd923382f166f3858dfabb8d0ebb4e29f62
Yahya Hussein <yahyahussein91@gmail.com>
2020-01-04 19:26:44 -0800

    done with first array challenge

---
+++ arrays/notes.js
@@ -0,0 +1,11 @@
+const notes = ["Note 1", "Note 2", "Note 3"];
+
+// Print out length and specific item
+console.log(notes.length);
+console.log(notes[notes.length - 1]);
+
+// First array challenge
+const todos = ["Get a job", "Get ready for nikkah", "Practice for interview", "Get big", "Find a wifey"];
+let sentence = `You have ${todos.length} todos`;
+console.log(sentence);
+console.log(todos[0], ' ', todos[4]);
