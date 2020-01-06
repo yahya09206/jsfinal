1747d5a76125962ea4a2f2d937db346ca5126371
Yahya Hussein <yahyahussein91@gmail.com>
2020-01-05 18:32:08 -0800

    im so dumb

--- todo/script.js
+++ todo/script.js
@@ -1,7 +1,47 @@
-const ps = document.querySelectorAll('p');
+const todos = [{
+	title: 'Goal for 2020',
+	body: 'Get a job',
+	completed: false
+}, {
+	title: 'Goal for January',
+	body: 'Get ready for nikkah',
+	completed: false
+}, {
+	title: 'Task for next couple of months',
+	body: 'Practice for interview',
+	completed: false
+}, {
+	title: 'Gym Goals',
+	body: 'Get big',
+	completed: true
+}, {
+	title: 'Longterm goal',
+	body: 'Find a wifey',
+	completed: false
+}];
 
-ps.forEach(function(p) {
-	if (p.textContent.includes('the')) {
-		p.remove();
-	}
-})
+//Check how many todos are not completed
+const incompleteTodos = todos.filter(function(todo) {
+	return !todo.completed;
+});
+
+const summary = document.createElement('h2');
+summary.textContent = `You have ${incompleteTodos.length} todos left`;
+document.querySelector('body').appendChild(summary);
+
+todos.forEach(function (todo) {
+	const p = document.createElement('p');
+	p.textContent = todo.title;
+	document.querySelector('body').appendChild(p);
+});
+
+
+
+
+
+
+
+
+
+
+
