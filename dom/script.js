ac036e9727640e913101855b814f437c28a52aa2
Yahya Hussein <yahyahussein91@gmail.com>
2020-01-08 17:44:47 -0800

    data is saving

--- dom/script.js
+++ dom/script.js
@@ -1,15 +1,16 @@
 // DOM - Document Object Model
-const notes = [{
-	title: 'My next move',
-	body: 'Secure this bag for real'
-}, {
-	title: 'Habits to work on',
-	body: 'Stop napping so much'
-}, {
-	title: 'Life modifications',
-	body: 'Do more work'
+// const notes = [{
+// 	title: 'My next move',
+// 	body: 'Secure this bag for real'
+// }, {
+// 	title: 'Habits to work on',
+// 	body: 'Stop napping so much'
+// }, {
+// 	title: 'Life modifications',
+// 	body: 'Do more work'
 
-}];
+// }];
+let notes = [];
 
 const filters = {
 	searchText: ''
@@ -19,7 +20,34 @@ const filters = {
 // localStorage.setItem('Location', 'Seattle');
 
 // Read getItem
-console.log(localStorage.getItem('Location'));
+// console.log(localStorage.getItem('Location'));
+
+// Delete previously saved data
+// localStorage.removeItem('Location');
+
+// Delete all data
+// localStorage.clear();
+
+// Save array of objects
+// const user = {
+// 	name: 'Charlie',
+// 	age: 29
+// }
+
+// const userJson = JSON.stringify(user);
+// console.log(userJson);
+// localStorage.setItem('user', userJson);
+
+// const userJSON = localStorage.getItem('user');
+// const user = JSON.parse(userJSON);
+// console.log(`${user.name} is ${user.age}`);
+
+//Check for existing saved data
+const notesJSON = localStorage.getItem('notes');
+
+if (notesJSON !== null) {
+	notes = JSON.parse(notesJSON);
+}
 
 
 const renderNotes = function(notes, filters){
@@ -32,7 +60,13 @@ const renderNotes = function(notes, filters){
 
 	filteredNotes.forEach(function(note) {
 		const noteElement = document.createElement('p');
-		noteElement.textContent = note.title;
+		//Show title if none exists
+		if (note.title.length > 0) {
+			noteElement.textContent = note.title;
+		}else {
+			noteElement.textContent = 'Unnamed note';
+		}
+		// noteElement.textContent = note.title;
 		document.querySelector('#notes').appendChild(noteElement);
 	})
 }
@@ -40,7 +74,14 @@ const renderNotes = function(notes, filters){
 renderNotes(notes, filters);
 
 document.querySelector('#create-note').addEventListener('click', function(e) {
-	e.target.textContent = 'The button was clicked';
+	//e.target.textContent = 'The button was clicked';
+	notes.push({
+		title: '',
+		body: ''
+	})
+	// Save new array to local storage
+	localStorage.setItem('notes', JSON.stringify(notes));
+	renderNotes(notes, filters);
 });
 
 document.querySelector('#search-text').addEventListener('input', function(e){
